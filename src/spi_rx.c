d8cb29b01407c1c1b92dfa78c6a933046cc3ad43
Nathan Huang <113065967+NHuang58@users.noreply.github.com>
2024-11-21 01:24:53 -0500

    Update spi_rx.c

--- src/spi_rx.c
+++ src/spi_rx.c
@@ -1,23 +1,43 @@
-#include "stm32f0xx.h"
+void init_uart_receiver(void) {
+    // Enable USART1 and GPIOA clocks
+    RCC->APB2ENR |= RCC_APB2ENR_USART1EN;  // Enable USART1 clock
+    RCC->AHBENR |= RCC_AHBENR_GPIOAEN;      // Enable GPIOA clock
 
+    // Configure PA10 as USART1 RX (Alternate Function)
+    GPIOA->MODER &= ~GPIO_MODER_MODE10;           // Clear the current mode for PA10
+    GPIOA->MODER |= GPIO_MODER_MODE10_1;          // Set PA10 to alternate function mode
+    GPIOA->AFR[1] &= ~GPIO_AFRH_AFSEL10;          // Clear the alternate function bits for PA10
+    GPIOA->AFR[1] |= 0x01;                       // Set PA10 to USART1_RX (AF1)
 
-void SPI_Init_Slave(void) {
-    // Enable SPI2 and GPIOB clock (Assuming SPI2 is used on GPIOB)
-    RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
-    RCC->AHBENR |= RCC_AHBENR_GPIOBEN;
+    // Set the baud rate (assuming 48 MHz system clock)
+    USART1->BRR = 0x683;  // Baud rate 9600
 
-    // Configure SPI2 pins: PB14 -> SCK, PB15 -> MISO, PB13 -> MOSI
-    GPIOB->MODER &= ~(GPIO_MODER_MODER13_Msk | GPIO_MODER_MODER14_Msk | GPIO_MODER_MODER15_Msk);
-    GPIOB->MODER |= (GPIO_MODER_MODER13_1 | GPIO_MODER_MODER14_1 | GPIO_MODER_MODER15_1); // Alternate function
-    GPIOB->AFR[1] |= (0x06 << (4*1)) | (0x06 << (4*2)) | (0x06 << (4*3)); // Set AF6 for SPI
+    // Configure USART1: 8 data bits, no parity, 1 stop bit
+    USART1->CR1 &= ~USART_CR1_M;       // 8 data bits
+    USART1->CR1 &= ~USART_CR1_PS;      // No parity
+    USART1->CR1 &= ~USART_CR1_OVER8;   // 16x oversampling mode
+    USART1->CR2 &= ~USART_CR2_STOP;    // 1 stop bit
 
-    // Configure SPI in Slave mode, full-duplex, 8-bit, SCK idle low, MSB first
-    SPI2->CR1 &= ~SPI_CR1_SPE;  // Disable SPI
-    SPI2->CR1 |= SPI_CR1_SSI | SPI_CR1_SSM | SPI_CR1_SPE;  // Slave mode, Software slave management, Enable SPI
+    // Enable USART1 for receiving
+    USART1->CR1 |= USART_CR1_RE;      // Enable receiver
+    USART1->CR1 |= USART_CR1_UE;      // Enable USART1
 }
 
-// Receive data using SPI
-uint8_t SPI_Receive_Char(void) {
-    while (!(SPI2->SR & SPI_SR_RXNE));  // Wait for RXNE (Receive buffer not empty)
-    return SPI2->DR;  // Read data from SPI data register
+// Function to receive a character
+char receive_char(void) {
+    while (!(USART1->SR & USART_SR_RXNE));  // Wait until data is received
+    return (char)(USART1->DR);  // Return the received character
+}
+
+int main(void) {
+    // Initialize USART receiver
+    init_uart_receiver();
+
+    while (1) {
+        // Receive a character from USART1
+        char received_char = receive_char();
+
+        // Process the received character (for example, echo it)
+        // Here we can either process it or store it
+    }
 }
